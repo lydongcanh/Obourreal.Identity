f1fc98dba30d62e78ce35c162ccb68a4322afea6
Ted Ly <lydongcanh@gmail.com>
2021-10-10 01:29:42 +0700

    Allow enter bearer token in swagger UI.

--- Obourreal.Identity.Api/ServiceConfiguration/SwaggerConfiguration.cs
+++ Obourreal.Identity.Api/ServiceConfiguration/SwaggerConfiguration.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace Obourreal.Identity.Api.ServiceConfiguration
 {
@@ -7,7 +13,32 @@ namespace Obourreal.Identity.Api.ServiceConfiguration
     {
         public static IServiceCollection AddSwagger(this IServiceCollection services)
         {
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Name = "JWT Authentication",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Description = "Bearer token",
+
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecurityScheme, Array.Empty<string>() }
+                });
+
+            });
             return services;
         }
 
